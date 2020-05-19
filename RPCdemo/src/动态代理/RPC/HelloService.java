999afc7e54247e7de60d3995df91e789d2944082
xuxiao <813035623@qq.com>
2020-05-19 23:43:23 +0800

    xuxiao RPC demo

---
+++ RPCdemo/src/动态代理/RPC/HelloService.java
@@ -0,0 +1,5 @@
+package ¶¯Ì¬´úÀí.RPC;
+
+public interface HelloService {
+	 String sayHi(String name);
+}
---
+++ RPCdemo/src/动态代理/RPC/HelloServiceImpl.java
@@ -0,0 +1,9 @@
+package ¶¯Ì¬´úÀí.RPC;
+
+public class HelloServiceImpl implements HelloService{
+
+	public String sayHi(String name) {
+		return "Hi, " + name;
+	}
+
+}
